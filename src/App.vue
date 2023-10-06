645cd2d9f6d18698ebf398e7550da649dce33924
Dmytro Ushchenko <dmytro.ushchenko@vaimo.com>
2023-10-06 18:24:24 +0300

    transitions added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <TheNavigation />
   <div class="container">
-    <router-view :key="$route.path" />
+    <!--    <router-view :key="$route.path" />-->
+    <router-view v-slot="{ Component }">
+      <transition name="moveUp">
+        <component :is="Component" :key="$route.path"></component>
+      </transition>
+    </router-view>
   </div>
 </template>
 <script>
@@ -14,7 +19,36 @@ export default {
 };
 </script>
 
-<style>
+<style lang="css">
+.moveUp-enter-active {
+  animation: fadeIn 1s ease-in;
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+.moveUp-leave-active {
+  animation: moveUp 0.3s ease-in;
+}
+
+@keyframes moveUp {
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(-400px);
+  }
+}
+
 .my-custom-active-link {
   background: #4e6c71;
   border-bottom: 1px solid black;
--- src/views/DestinationView.vue
+++ src/views/DestinationView.vue
@@ -1,33 +1,35 @@
 <template>
-  <section>
-    <h2>{{ destination.name }}</h2>
-    <a href=""></a>
-    <img
-      :src="getImage(destination.image ?? 'waikiki-beach.jpg')"
-      :alt="destination.name"
-    />
-    <p>{{ destination.description }}</p>
-  </section>
-  <section class="experiences">
-    <div class="cards">
-      <router-link
-        v-for="experience in destination.experiences"
-        :key="experience.slug"
-        :to="{
-          name: 'experience.show',
-          params: {
-            id: destination.id,
-            slug: destination.slug,
-            experienceSlug: experience.slug,
-          },
-        }"
-      >
-        <h3>{{ experience.name }}</h3>
-        <img :src="getImage(experience.image)" :alt="experience.name" />
-      </router-link>
-    </div>
-  </section>
-  <router-view />
+  <div>
+    <section>
+      <h2>{{ destination.name }}</h2>
+      <a href=""></a>
+      <img
+        :src="getImage(destination.image ?? 'waikiki-beach.jpg')"
+        :alt="destination.name"
+      />
+      <p>{{ destination.description }}</p>
+    </section>
+    <section class="experiences">
+      <div class="cards">
+        <router-link
+          v-for="experience in destination.experiences"
+          :key="experience.slug"
+          :to="{
+            name: 'experience.show',
+            params: {
+              id: destination.id,
+              slug: destination.slug,
+              experienceSlug: experience.slug,
+            },
+          }"
+        >
+          <h3>{{ experience.name }}</h3>
+          <img :src="getImage(experience.image)" :alt="experience.name" />
+        </router-link>
+      </div>
+    </section>
+    <router-view />
+  </div>
 </template>
 <script>
 import axios from "axios";
